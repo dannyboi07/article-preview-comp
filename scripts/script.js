416c670e599bf30bd55cdded912b8a0e3696ef88
dannyboi07 <48730475+dannyboi07@users.noreply.github.com>
2022-01-31 22:44:26 +0530

    Add files via upload

--- scripts/script.js
+++ scripts/script.js
@@ -4,10 +4,18 @@ const shareCtn = document.getElementById("share-ctn");
 const shareSvg = document.getElementById("share-svg");
 const bloggerDetails = document.getElementById("blogger-details");
 const profileCtn = document.getElementById("profile-ctn");
+const icon = document.querySelectorAll("#icon-pop");
 
 shareBtn.addEventListener("click", () => {
     shareCtn.classList.toggle("share-ctn-animate-addon");
     shareBtn.classList.toggle("share-btn-color");
     shareSvg.classList.toggle("share-svg-color-active");
-    if (screen.width < 1028) profileCtn.classList.toggle("profile-container-inactive");
+    if (window.innerWidth < 1280) profileCtn.classList.toggle("profile-container-inactive");
+    if (window.innerWidth > 1280) {
+        // console.log(icon);
+        Array.from(icon).forEach(ic => ic.classList.toggle("icon-pop-anim"));
+    } else {
+        Array.from(icon).forEach(ic => ic.classList.toggle("icon-pop-anim-mob"));
+
+    }
 });
